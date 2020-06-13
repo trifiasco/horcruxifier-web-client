69887129515a932054b802dd25c7a99448a9dbc2
Arnab Roy <trifiasco@gmail.com>
2020-06-14 04:25:57 +0600

    Add support for any file extension

--- src/components/utils/dialog/dialog-container.js
+++ src/components/utils/dialog/dialog-container.js
@@ -4,6 +4,12 @@ import download from 'js-file-download';
 import DialogView from './dialog-view';
 
 const DialogContainer = props => {
+    const getFileExtension = (file) => {
+        console.log(file);
+        const parts = file.name.split('.');
+        return parts.pop();
+      }
+
     const cleanUp = async (url, filename) => {
         const response = await sendRequest('DELETE', url, null, {filename : filename});
         return response;
@@ -24,24 +30,28 @@ const DialogContainer = props => {
 
     const onUpload = async (selectedFile, password, variant) => {
       const data = new FormData();
-      let baseUrl;
+      let baseUrl, fileExtension;
       const serverUrl = process.env.NODE_ENV === 'development' ? `http://localhost:5001` : ''
       if(variant === 'horcruxify'){
           data.append('file', selectedFile.selectedFile);
           baseUrl = `${serverUrl}/horcruxify`
+          fileExtension = getFileExtension(selectedFile.selectedFile);
+          data.append('fileExtension', fileExtension);
       }
       else{
           for(var x = 0; x < selectedFile.selectedFile.length; x++){
               data.append('file', selectedFile.selectedFile[x]);
           }
-          baseUrl = `${serverUrl}/dehorcruxify`
+          baseUrl = `${serverUrl}/dehorcruxify`;
+          fileExtension = getFileExtension(selectedFile.selectedFile[0]);
+          data.append('fileExtension', fileExtension);
       }
       data.append('password', password);
       const res = await sendRequest('POST', baseUrl, data, {});
       if(res.status === 200){
           const response = await sendRequest('GET', baseUrl, null, {filename: res.data});
           const fileBlob = await response.blob();
-          download(fileBlob, variant === 'horcruxify' ? 'horcruxes.zip' : 'original.txt');
+          download(fileBlob, variant === 'horcruxify' ? 'horcruxes.zip' : `original.${fileExtension}`);
           console.log('file should be downloaded by now!!');
           await cleanUp(baseUrl, res.data);
       }
